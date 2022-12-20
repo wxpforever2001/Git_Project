5892da0bca97bdd95de73dc50e673666c1bf4b72
빛 <22sunrin099@sunrint.hs.kr>
2022-12-20 11:31:26 +0900

    정답 확인 코드

--- random.c
+++ random.c
@@ -1,7 +1,9 @@
+const char op[] = { 0, '+', '-', '*', '/' };
+
 f(i)
 	int i;
 {
-	int x, y, o, ans;
+	int x, y, o, ans, inp;
 	if(i > 10) return 0;
 	x = rand() % 20 + 1;
 	y = rand() % 20 + 1;
@@ -10,6 +12,14 @@ f(i)
 	else if(o == 2) ans = x - y;
 	else if(o == 3) ans = x * y;
 	else if(o == 4) ans = x / y;
+	
+	printf("[%i/10] %i %c %i = ", i, x, op[o], y);
+	scanf("%i", &inp);
+	if(ans == inp) {
+		puts("Correct!");
+	} else {
+		printf("Wrong! Answer is %i.\n", ans);
+	}
 }
 
 main()
